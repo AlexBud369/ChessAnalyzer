df2d698f5bb66a4da1f37d1350f36116c825c127
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-04-20 19:37:42 +0300

    feat: add function for digit evaluation parser

--- src/scripts/evaluation_parser.py
+++ src/scripts/evaluation_parser.py
@@ -1,11 +1,12 @@
 from typing import Optional
 
+
 def _parse_mate(eval_str: str) -> Optional[float]:
     """
-    Обрабатывает матовые оценки вида #+2, #-3,
-    Возвращает +-30 условное значение для мата
+    Обрабатывает матовые оценки вида #+2, #-3.
+    Возвращает ±30 (условное значение для мата).
     """
-    rest = eval_str[1:]  # убираем '#'
+    rest = eval_str[1:]
     sign = 1
     if rest.startswith('+'):
         rest = rest[1:]
@@ -21,13 +22,24 @@ def _parse_mate(eval_str: str) -> Optional[float]:
 
 def _parse_centipawns(eval_str: str) -> Optional[float]:
     """
-    Преобразует сантипешки в пешки (строки по типу +1234, -50)
+    Преобразует сантипешки в пешки (строки по типу +1234, -50).
     """
     if eval_str.startswith('+'):
         eval_str = eval_str[1:]
     try:
         centipawns = int(eval_str)
         return centipawns / 100.0
+    except ValueError:
+        return None
+
+
+def _parse_direct_pawns(eval_str: str) -> Optional[float]:
+    """
+    Парсит строки, содержащие просто число (с плавающей точкой или целое)
+    как оценку в пешках (по типу2.0, -4, 0.5, 7)
+    """
+    try:
+        return float(eval_str)
     except ValueError:
         return None
 
@@ -42,5 +54,8 @@ def parse_evaluation(eval_str: str) -> Optional[float]:
 
     if eval_str.startswith('#'):
         return _parse_mate(eval_str)
-    else:
+
+    if eval_str[0] in ('+', '-'):
         return _parse_centipawns(eval_str)
+
+    return _parse_direct_pawns(eval_str)
