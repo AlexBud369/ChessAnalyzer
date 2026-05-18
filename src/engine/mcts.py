f3cf53c8234c2aec3f9f2b2bdf5d5b48d56bc810
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2026-05-18 11:01:54 +0300

    feat: rewrite MCTS algorithm

--- src/engine/mcts.py
+++ src/engine/mcts.py
@@ -30,34 +30,116 @@ class MCTSNode:
                 best_child = child
         return best_child
 
+
 class MCTS:
-    def __init__(self, evaluator, num_iterations=800):
+    def __init__(self, evaluator, num_iterations=400, c_puct=1.4):
         self.evaluator = evaluator
         self.num_iterations = num_iterations
+        self.c_puct = c_puct
+        self.reset_tree()
 
-    def search(self, root_board: chess.Board):
-        root = MCTSNode(root_board)
+    def reset_tree(self):
+        self.Qsa = {}
+        self.Nsa = {}
+        self.Ns = {}
+        self.Ps = {}
+        self.Vs = {}
+        self.Es = {}
+
+    def _hash_board(self, board: chess.Board) -> str:
+        fen_parts = board.fen().split(' ')
+        base_fen = ' '.join(fen_parts[:4])
+        return base_fen
+
+    def _action_to_idx(self, move: chess.Move) -> int:
+        return move.from_square * 64 + move.to_square
+
+    def search(self, board: chess.Board):
+        s = self._hash_board(board)
+
+        if board.is_game_over():
+            if board.result() == "1-0":
+                v = 1.0
+            elif board.result() == "0-1":
+                v = -1.0
+            else:
+                v = 0.0
+            self.Es[s] = v
+            return v
+
+        if s not in self.Ps:
+            result = self.evaluator.evaluate(board)
+            value, policy_probs = result
+            legal_probs = legal_policy_probs(policy_probs, board)
+            self.Ps[s] = legal_probs
+            self.Vs[s] = value
+            self.Ns[s] = 0
+            return value
+
+        best_ucb = -float('inf')
+        best_action = None
+        for action in board.legal_moves:
+            a = self._action_to_idx(action)
+            q = self.Qsa.get((s, a), 0.0)
+            u = self.c_puct * self.Ps[s][a] * math.sqrt(self.Ns[s]) / (1 + self.Nsa.get((s, a), 0))
+            ucb = q + u
+            if ucb > best_ucb:
+                best_ucb = ucb
+                best_action = action
+
+        next_board = board.copy()
+        next_board.push(best_action)
+        v = self.search(next_board)
+
+        a = self._action_to_idx(best_action)
+        self.Nsa[(s, a)] = self.Nsa.get((s, a), 0) + 1
+        self.Ns[s] += 1
+        old_q = self.Qsa.get((s, a), 0.0)
+        self.Qsa[(s, a)] = old_q + (v - old_q) / self.Nsa[(s, a)]
+
+        return v
+
+    def get_best_move(self, board: chess.Board):
+        self.reset_tree()
         for _ in range(self.num_iterations):
-            node = root
+            self.search(board)
+        s = self._hash_board(board)
+        best_action = None
+        best_visits = -1
+        for action in board.legal_moves:
+            a = self._action_to_idx(action)
+            visits = self.Nsa.get((s, a), 0)
+            if visits > best_visits:
+                best_visits = visits
+                best_action = action
+        return best_action
 
-            while node.is_expanded():
-                node = node.select_child()
+    def get_policy_prob(self, board: chess.Board, temperature=1.0):
+        self.reset_tree()
+        for _ in range(self.num_iterations):
+            self.search(board)
+        s = self._hash_board(board)
+        policy = {}
+        moves = list(board.legal_moves)
+        if temperature == 0:
+            best_move = max(moves, key=lambda m: self.Nsa.get((s, self._action_to_idx(m)), 0))
+            for move in moves:
+                policy[move] = 1.0 if move == best_move else 0.0
+        else:
+            visits = np.array([self.Nsa.get((s, self._action_to_idx(m)), 0) for m in moves], dtype=np.float32)
+            if np.sum(visits) == 0:
+                probs = np.ones(len(moves)) / len(moves)
+            else:
+                probs = visits ** (1.0 / temperature) / np.sum(visits ** (1.0 / temperature))
+            for move, prob in zip(moves, probs):
+                policy[move] = prob
+        return policy
 
-            value, policy_probs = self.evaluator.evaluate(node.board)
-            legal_probs = legal_policy_probs(policy_probs, node.board)
+    def sample_move(self, board: chess.Board, temperature=1.0):
+        policy = self.get_policy_prob(board, temperature=temperature)
+        moves = list(policy.keys())
+        probs = np.array([policy[m] for m in moves])
 
-            for move in node.board.legal_moves:
-                idx = move.from_square * 64 + move.to_square
-                prior = legal_probs[idx]
-                if prior > 1e-6:
-                    new_board = node.board.copy()
-                    new_board.push(move)
-                    node.children[move] = MCTSNode(new_board, parent=node, prior=prior)
-
-            while node is not None:
-                node.visits += 1
-                node.value_sum += value
-                node = node.parent
-
-        best_move = max(root.children.items(), key=lambda kv: kv[1].visits)[0]
-        return best_move
+        probs = probs / np.sum(probs)
+        chosen_idx = np.random.choice(len(moves), p=probs)
+        return moves[chosen_idx]
